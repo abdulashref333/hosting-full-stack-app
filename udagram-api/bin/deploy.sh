da02749fe49620022693787b89c7fc12505cc8ec
Abdulrahman <abdul.ashref333@gmail.com>
2022-03-11 00:35:32 +0200

    up back end deploy script

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
 eb init udgram-api --platform node.js --region us-east-1
 eb create --sample udgram-dev
-eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="postgres" POSTGRES_DB="postgres" PORT_DB="5432" POSTGRES_HOST="database-2.cgmysk7pochc.us-east-1.rds.amazonaws.com" AWS_REGION="us-east-1" AWS_PROFILE="default" AWS_BUCKET="udagram-frontend22" URL="localhost:8080" JWT_SECRET="22"
+eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="postgres" POSTGRES_DB="database-2" PORT_DB="5432" POSTGRES_HOST="database-2.cgmysk7pochc.us-east-1.rds.amazonaws.com" AWS_REGION="us-east-1" AWS_PROFILE="default" AWS_BUCKET="udagram-frontend22" URL="localhost:8080" JWT_SECRET="22"
 eb deploy
