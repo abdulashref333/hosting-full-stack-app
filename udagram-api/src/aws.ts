211635e221ebe41a145ed516a1f659a92ee85dd5
Abdulrahman <abdul.ashref333@gmail.com>
2022-03-11 17:16:46 +0200

    add config

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -2,9 +2,14 @@ import AWS = require("aws-sdk");
 import { config } from "./config/config";
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
-AWS.config.credentials = credentials;
-
+// const credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
+// AWS.config.credentials = credentials;
+AWS.config = new AWS.Config({
+  accessKeyId: "AKIA3ZAEOW36KT2MMKGK",
+  secretAccessKey: "noI8WVYbmubSH3a9cJkqJ+IZ1GzJc4tYL/6ffjI",
+  region: "us-east-1",
+  signatureVersion: "v4",
+});
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
